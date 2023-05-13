f84553981f99d2e302c0ae56781ef7b90ccdb573
Katie Brown <katieohalloranbrown@gmail.com>
2023-05-13 13:55:07 -0400

    Add path line

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,23 +1,49 @@
 import mapboxgl from "mapbox-gl";
 import React, { useEffect, useState } from "react";
+import { mapPathGeoJSON } from "../constants";
 
 const token: string | undefined = process.env.REACT_APP_MAPBOXKEY
 if (token) {mapboxgl.accessToken = token}
 
-const phillyCoordinates: [number, number] = [-76.6122, 39.2904];
-
 const Map = () => {
   const [map, setMap] = useState<mapboxgl.Map | null>(null);
+
+  // TODO #11: replace currentProgress with actual value from backend
+  const currentProgress: [number, number] = [29.016474, 41.021126];
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       style: "mapbox://styles/mapbox/streets-v11",
-      center: phillyCoordinates,
-      zoom: 11.5,
+      center: currentProgress,
+      zoom: 9,
       container: "map-container",
     });
     setMap(map);
   }, []);
+
+  useEffect(() => {
+    if (map) {
+      map.on('load', () => {
+        map.addSource('path', {
+          'type': 'geojson',
+          'data': mapPathGeoJSON
+        })
+      
+      map.addLayer({
+        'id': 'path',
+        'type': 'line',
+        'source': 'path',
+        'paint': {
+          'line-color': '#CDC1BF',
+          'line-width': 4
+        }
+      })
+    })
+      new mapboxgl.Marker()
+        .setLngLat(currentProgress)
+        .addTo(map)
+    }
+  }, [map])
 
 
   return <div id="map-container" style={{ height: "800px" }}></div>;
